6833d26769877239560690e2da7a48299cedfad6
Phillip Malboeuf <phil@phils.computer>
2023-12-02 13:31:31 -0500

    vercel edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+  output: "server",
+  adapter: vercel({
+    edgeMiddleware: true
+  }),
   image: {
     domains: ["images.ctfassets.net", "downloads.ctfassets.net"]
   },
